e3225329ebfe30a80c11bfa8b97f9bc142544016
Jason Huang <cjhuang@yahoo-inc.com>
2015-02-01 14:50:41 +0800

    add template

--- app/jsx/Sop.jsx
+++ app/jsx/Sop.jsx
@@ -1,8 +1,27 @@
+/* jshint ignore:start */
 /** @jsx React.DOM */
+
+var SopItem = React.createClass({
+    render: function () {
+        return (
+            <li class="sop-item">
+                <img src="http://placekitten.com/g/135/135" alt="" />
+                <h1>卸甲歸鄉</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+        );
+    }
+});
 
 var Sop = React.createClass({
     render: function () {
-        return <div>React.js</div>;
+        return (
+            <div className="sop-viewport">
+                <div className="sop-container">
+                    {[<SopItem />, <SopItem />, <SopItem />, <SopItem />, <SopItem />]}
+                </div>
+            </div>
+        );
     }
 });
 
